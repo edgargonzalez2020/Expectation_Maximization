c1da68efbfeb10e62ee90ee31cbc7e0032064b5a
edgargonzalez2020 <edgar.gonzalez2@mavs.uta.edu>
2019-12-06 00:20:39 -0600

    Update em_cluster.py

--- em_cluster.py
+++ em_cluster.py
@@ -1,100 +1,106 @@
-from operator import add
+# Edgar Gonzalez
+# 1001336686
 import sys
 import random
 import numpy as np
-from scipy.stats import multivariate_normal
-
 class Factory:
     def __init__(self, path):
-        self.path = path
         self.data = np.loadtxt(path)[:,:-1]
         self.rows = self.data.shape[0]
         self.cols = self.data.shape[1]
-class Cluster:
-    def __init__(self, clusters, epoch, factory):
-        self.clusters = int(clusters)
-        self.epochs = int(epoch)
+class EmCluster:
+    def __init__(self, number_of_clusters, iterations, factory):
+        self.number_of_clusters = int(number_of_clusters)
+        self.iterations = int(iterations)
         self.factory = factory
-        self.init_params()
-        self.train()
-    def init_params(self):
-        self.prob = [[0 for _ in range(self.clusters)] for _ in range(self.factory.rows)]
-        for i in range(len(self.prob)):
-            self.prob[i][random.randint(0, self.clusters - 1)] = 1
     def train(self):
-        for _ in range(self.epochs):
+        self.prob = [[0 for _ in range(self.number_of_clusters)] for _ in range(self.factory.rows)]
+        for x in range(self.factory.rows):
+            self.prob[x][random.randint(0, self.number_of_clusters - 1)] = 1
+        for i in range(1,self.iterations + 1):
             means, weights, covariances = self.m_step()
             self.e_step(means, weights, covariances)
-        with open("points.txt", "w") as f:
-             for i,x in enumerate(self.factory.data):
-                 f.write(f"{x[0]} {x[1]}")
-                 for j in range(self.clusters):
-                     f.write(f" {self.prob[i][j]}")
-                 f.write("\n")
+            print(f"After iteration {i}: ")
+            count = 1
+            for y in range(self.number_of_clusters):
+                inner = ",".join(str(np.around(x, decimals=4)) for x in means[y])
+                print(f"weight {count} = {weights[y]:.4f}, mean = ({inner})")
+                count += 1
+        print("After final iteration")
+        count = 1
+        for i in range(self.number_of_clusters):
+            inner = ",".join(str(np.around(x, decimals=4)) for x in means[i])
+            print(f"weight {count} = {weights[i]:.4f}, mean {i} = ({inner})")
+            for j,y in enumerate(covariances[i],start=1):
+                matrix = ",".join([str(np.around(x,decimals=4)) for x in y])
+                print(f"Sigma {count} row {j} = ({matrix})")
+            count += 1
     def e_step(self, means, weights, covariances):
         bottom = []
         probabilities = []
         for x in self.factory.data:
             total = 0
             prob_j = []
-            for i in range(self.clusters):
-                gaussian = self.multivariate_gaussian(np.array(means[i]), np.array(covariances[i]), x)
+            for i in range(self.number_of_clusters):
+                gaussian = self.multivariate_gaussian(x,np.array(means[i]), np.array(covariances)[i])
                 final = gaussian * weights[i]
                 prob_j.append(final)
                 total += final
             prob_j = [x/total for x in prob_j]
             probabilities.extend([prob_j])
         self.prob = probabilities
-    def m_step(self):
-        probabilities = np.array(self.prob)
-        weights = []
-        N_k = []
-        for x in range(self.clusters):
-            column_sum = np.sum(probabilities[:,x])
-            array_sum = probabilities.sum()
-            weights.append(column_sum / array_sum)
-            N_k.append(column_sum)
-        means = []
-        for i in range(self.clusters):
-            pij = N_k[i]
-            temp = np.zeros((1,self.factory.cols))
-            for x in self.factory.data:
-                temp += pij * x
-            temp /= pij
-            means.extend(temp)
-        means = np.array(means).tolist()
-        covariances = []
-        means = np.array(means)
-        for cluster in range(self.clusters):
-            temp = np.zeros((self.factory.cols, self.factory.cols))
-            for i, x in enumerate(self.factory.data):
-                prob = self.prob[i][cluster]
-                means_cluster = means[cluster, :].reshape(1,self.factory.cols)
-                first = (x - means_cluster).T.reshape(self.factory.cols, 1)
-                second = (x - means_cluster).reshape(1, self.factory.cols)
-                final = prob * (first @ second)
-                temp += final
-            if N_k[cluster != 0]:
-                    temp = temp / N_k[cluster]
-            for r in range(self.factory.cols):
-                for x in range(self.factory.cols):
-                    if r == x and temp[r][x] < 0.0001:
-                        print("GOT ONE")
-                        temp[r][x] = 0.0001
-            covariances.extend([temp])
-        covariances = np.array(covariances).tolist()
-        means = means.tolist()
-        return means, weights, covariances
-    def multivariate_gaussian(self, mean, covariance, x):
-        first = 1 / ( np.sqrt( np.power(2 * np.pi, self.factory.cols) * np.linalg.det(covariance) ) )
-        second = np.exp( -0.5 * (x - mean).T @ np.linalg.pinv(covariance) @ (x - mean) )
+        for i in range(len(probabilities)):
+            total = sum(probabilities[i])
+            for j in range(len(probabilities[i])):
+                probabilities[i][j] /= total
+    def multivariate_gaussian(self, x, mean,covariance):
+        d = self.factory.cols
+        first = 1 / ( np.sqrt( np.power(2 * np.pi, d) * np.linalg.det(covariance) ) )
+        second = np.exp( -0.5 * (x - mean).T @ np.linalg.pinv(covariance) @ (x - mean))
         return first * second
-        var = multivariate_normal(mean = mean, cov = covariance)
-        return var.pdf(x)
+    def m_step(self):
+        weights = [0 for _ in range(self.number_of_clusters)]
+        means = []
+        pij = [0 for _ in range(self.number_of_clusters)]
+        covariances = []
+        denominator = 0
+        for cluster in range(self.number_of_clusters):
+            numerator = 0
+            for i, x in enumerate(self.factory.data):
+                 numerator += self.prob[i][cluster]
+                 denominator += self.prob[i][cluster]
+            pij[cluster] = numerator
+            weights[cluster] = numerator
+        weights = [x/denominator for x in weights]
+        self.prob = np.array(self.prob)
+        for cluster in range(self.number_of_clusters):
+            numerator = np.zeros((1, self.factory.cols))
+            for i,x in enumerate(self.factory.data):
+                prob = self.prob[i][cluster]
+                numerator += prob * np.array(x)
+            means.extend(numerator / pij[cluster])
+        means = np.array(means).tolist()
+        for cluster in range(self.number_of_clusters):
+            temp = [[0 for _ in range(self.factory.cols)] for _ in range(self.factory.cols)]
+            for r in range(self.factory.cols):
+                for c in range(self.factory.cols):
+                    prob = self.prob[:,cluster]
+                    mu_r = means[cluster][r]
+                    mu_c = means[cluster][c]
+                    x_r = self.factory.data[:,r] - mu_r
+                    x_c = self.factory.data[:,c] - mu_c
+                    upper = np.sum(prob * x_r * x_c)
+                    if r == c and (upper / pij[cluster]) < 0.0001:
+                        temp[r][c] = 0.0001
+                    else:
+                        temp[r][c] = upper / pij[cluster]
+            covariances.append(temp)
+        return means, weights, covariances
 def main():
     if len(sys.argv) < 4:
-        print("usage: [path to file] k number_of_iterations")
-        return
-    clusters = Cluster(*sys.argv[2:4], Factory(sys.argv[1]))
+        print("usage: [path_to_file] k iterations")
+    factory = Factory(sys.argv[1])
+    cluster = EmCluster(*sys.argv[2:5], factory)
+    cluster.train()
 if __name__ == "__main__":
     main()
